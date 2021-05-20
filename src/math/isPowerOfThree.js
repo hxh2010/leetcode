e297e48b44e63ef7ea5f1d6dc5d25eece6a03968
hongxh <hongxh@vanke.com>
2021-05-20 13:50:41 +0800

    change name

--- src/math/isPowerOfThree.js
+++ src/math/isPowerOfThree.js
@@ -3,22 +3,23 @@
  * @return {boolean}
  */
 var isPowerOfThree = function (n) {
-  // 方法1
-  // if (n <= 0) return false;
-  // if (n === 1) return true;
-  //
-  // const fn = (m) => {
-  //   if (m === 1) return true;
-  //   const x = m % 3;
-  //   if (x === 0) return fn(m / 3);
-  //   return false;
-  // };
-  // return fn(n);
+  if (n <= 0) return false;
+  if (n === 1) return true;
 
-  // 方法2
-  //  return n > 0 && (n == 1 || (n % 3 == 0 && isPowerOfThree(n / 3)));
+  const fn = (m) => {
+    if (m === 1) return true;
+    const x = m % 3;
+    if (x === 0) return fn(m / 3);
+    return false;
+  };
+  return fn(n);
+};
 
-  // 方法3
+var isPowerOfThree2 = function (n) {
+  return n > 0 && (n === 1 || (n % 3 === 0 && isPowerOfThree(n / 3)));
+};
+
+var isPowerOfThree3 = function (n) {
   const x = Math.log10(n);
   const y = Math.log10(3);
   console.log(x, y, x / y);
